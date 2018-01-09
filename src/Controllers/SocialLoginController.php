72f4816f5e131e506b33b9487e790767a5443a44
Steve Harmeyer <harmeyersteve@gmail.com>
2018-01-09 16:12:46 -0700

    updates

--- src/Controllers/SocialLoginController.php
+++ src/Controllers/SocialLoginController.php
@@ -4,9 +4,20 @@ namespace Submtd\SocialLogin\Controllers;
 
 use Illuminate\Routing\Controller;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class SocialLoginController extends Controller
 {
+    use RegistersUsers;
+
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = config('auth.providers.users.model');
+    }
+
     public function redirectToProvider($provider)
     {
         return Socialite::driver($provider)->redirect();
@@ -14,7 +25,16 @@ class SocialLoginController extends Controller
 
     public function handleProviderCallback($provider)
     {
-        $user = Socialite::driver($provider)->user();
-        dd($user);
+        $socialUser = Socialite::driver($provider)->user();
+        $name = $socialUser->getName();
+        $email = $socialUser->getEmail();
+        if (!$user = $this->model::where('email', $email)->first()) {
+            $user = new $this->model();
+            $user->name = $name;
+            $user->email = $email;
+            return redirect()->route('register', ['name' => $name, 'email' => $email]);
+        }
+        Auth::loginUsingId($user->id);
+        return redirect()->intended($this->redirectPath());
     }
 }
