155e01a866a3ffbe6c5315a0c5695829d188d6f3
Steve Harmeyer <harmeyersteve@gmail.com>
2018-01-09 16:24:40 -0700

    handle logins and registrations

--- src/Controllers/SocialLoginController.php
+++ src/Controllers/SocialLoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Routing\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
 
 class SocialLoginController extends Controller
 {
@@ -29,10 +31,13 @@ class SocialLoginController extends Controller
         $name = $socialUser->getName();
         $email = $socialUser->getEmail();
         if (!$user = $this->model::where('email', $email)->first()) {
-            $user = new $this->model();
-            $user->name = $name;
-            $user->email = $email;
-            return redirect()->route('register', ['name' => $name, 'email' => $email]);
+            event(new Registered($user = $this->model::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make(str_random(32)),
+            ])));
+            $this->guard()->login($user);
+            return redirect()->intended($this->redirectPath());
         }
         Auth::loginUsingId($user->id);
         return redirect()->intended($this->redirectPath());
