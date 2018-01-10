4fcaf8c979b408cedf136b739a3bb139ba4fbd43
Steve Harmeyer <harmeyersteve@gmail.com>
2018-01-09 17:16:38 -0700

    changed routes to /auth/social/callback/{provider} and /auth/social/{provider}

--- src/Routes.php
+++ src/Routes.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('login/social/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@redirectToProvider');
-    Route::get('login/social/{provider}/callback', '\Submtd\SocialLogin\Controllers\SocialLoginController@handleProviderCallback');
+    Route::get('auth/social/callback/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@handleProviderCallback');
+    Route::get('auth/social/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@redirectToProvider');
 });
