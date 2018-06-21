be9bb0b993e3bf86709e5a1b77a32c056065db90
Steve Harmeyer <harmeyersteve@gmail.com>
2018-06-21 12:50:40 -0700

    added route names

--- src/Routes.php
+++ src/Routes.php
@@ -5,7 +5,7 @@
  */
 Route::group(['middleware' => ['web']], function () {
     // handle callbacks from the providers
-    Route::get('auth/social/callback/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@handleProviderCallback');
+    Route::get('auth/social/callback/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@handleProviderCallback')->name('social.callback');
     // redirects the user to the provider
-    Route::get('auth/social/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@redirectToProvider');
+    Route::get('auth/social/{provider}', '\Submtd\SocialLogin\Controllers\SocialLoginController@redirectToProvider')->name('social.redirect');
 });
